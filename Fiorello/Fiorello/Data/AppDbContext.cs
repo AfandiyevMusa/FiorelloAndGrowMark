eeab139cd4c461da911a1ad0e36acc5ec29485a8
AfandiyevMusa <113282393+AfandiyevMusa@users.noreply.github.com>
2023-05-12 03:24:03 +0400

    hw is half ready

---
+++ Fiorello/Fiorello/Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Fiorello.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fiorello.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> option) : base(option)
+        {
+        }
+        public DbSet<Slider> sliders { get; set; }
+        public DbSet<SlidersInfo> slidersInfos { get; set; }
+        public DbSet<Blog> blogs { get; set; }
+        public DbSet<Experts> experts { get; set; }
+        public DbSet<Valentine> valentines { get; set; }
+        public DbSet<About> abouts { get; set; }
+        public DbSet<Instagram> instagrams { get; set; }
+        public DbSet<Category> categories { get; set; }
+        public DbSet<Product> products { get; set; }
+        public DbSet<Image> images { get; set; }
+        public DbSet<Discount> discounts { get; set; }
+    }
+}
+
